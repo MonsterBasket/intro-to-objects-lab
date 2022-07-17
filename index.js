dd69c2129c8cb2d4a26ec342452fd3d09b798dfe
MonsterBasket <blazza1@hotmail.com>
2022-07-17 13:13:55 +1000

    test complete
    Still struggling to establish when to use [] or not when passing variables into object commands.  Using [] also changes the notation required (ie. key:'value' vs [key]:value and key.'value' vs key[value]

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+const employee = {name: 'Fred', streetAddress: '1 Smith St'};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign({}, employee, {[key]:value})
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign(employee, {[key]:value})
+}
+function deleteFromEmployeeByKey(employee, key){
+    let newEmployee = Object.assign({}, employee);
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    let newEmployee = employee;
+    delete newEmployee[key];
+    return newEmployee;
+}
